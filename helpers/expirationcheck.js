38e9574e4e7a82a3341ec4850ae3d1cc14bfc2e0
Nathan Opresnik <nopresnik@me.com>
2021-03-03 12:58:06 +1100

    Add documentation

--- helpers/expirationcheck.js
+++ helpers/expirationcheck.js
@@ -1,3 +1,4 @@
 module.exports = () => {
+  // TODO: Add logic to check for bets which have passed their expiry date/time and mark them as ended.
   console.log("Checking for any bets that have expired....");
 };
