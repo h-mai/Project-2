c97174c9403a00ebd4b2e1579fa8600b77891b7c
Justin Reid <JayArghArgh@users.noreply.github.com>
2021-03-03 18:01:55 +1030

    Update server.js
    replaced single quotes with doubles

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Set Handlebars
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
