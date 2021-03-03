159bbd6fd2f8be41733c9024680b38eecbaad338
Justin Reid <JayArghArgh@users.noreply.github.com>
2021-03-03 18:16:33 +1030

    Update bet-api-routes.js
    removed a freaking space :/

--- routes/bet-api-routes.js
+++ routes/bet-api-routes.js
@@ -4,8 +4,8 @@ const db = require("../models");
 module.exports = app => {
   app.get("/bets", (req, res) => {
     db.Bet.findAll({
-      order: [["expires", "DESC"],],
-    }).then( allBets => {
+      order: [["expires", "DESC"]]
+    }).then(allBets => {
       res.render("bets", {
         data: allBets
       });
