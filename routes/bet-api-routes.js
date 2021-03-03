6eea8872ecad348dfef9110c95d5175a1250ebdc
jayarghargh <jayarghargh@gmail.com>
2021-03-03 21:22:06 +1030

    added single bet view

--- routes/bet-api-routes.js
+++ routes/bet-api-routes.js
@@ -2,24 +2,41 @@
 const db = require("../models");
 
 module.exports = app => {
+  // For all bets
   app.get("/bets", (req, res) => {
     db.Bet.findAll({
       order: [["expires", "DESC"]]
     }).then(allBets => {
-      res.render("bets", {
+      res.render("all-bets", {
         data: allBets
       });
     });
   });
 
-  app.get("/api/bets/:catId", (req, res) => {
+  // For filtered bets
+  app.get("/bets/:catId", (req, res) => {
     db.Bet.findAll({
       where: {
         catId: req.params.catId
       }
     }).then(allBets => {
-      res.render("bets", {
+      res.render("all-bets", {
         data: allBets
+      });
+    });
+  });
+
+  // For a single bet
+  app.get("/bet/:betId", (req, res) => {
+    db.Bet.findOne({
+      where: {
+        id: req.params.betId
+      },
+      raw: true
+    }).then(oneBet => {
+      console.log(oneBet)
+      res.render("one-bet", {
+        data: oneBet
       });
     });
   });
