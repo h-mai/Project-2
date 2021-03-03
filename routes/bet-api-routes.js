f176b604eebeaee2f3ad2317132e988dcaecd775
jayarghargh <jayarghargh@gmail.com>
2021-03-03 15:46:41 +1030

    implemented bet-api-routes through to handlebars template

--- routes/bet-api-routes.js
+++ routes/bet-api-routes.js
@@ -1,11 +1,15 @@
 // API routes required for bet management.
 const db = require("../models");
 
-module.exports = app => {
-  app.get("/api/bets", (req, res) => {
-    db.Bet.findAll({}).then(allBets => {
+module.exports = (app) => {
+  app.get("/bets", (req, res) => {
+    db.Bet.findAll({
+      order: [
+        ['expires', 'DESC'],
+      ],
+    }).then((allBets) => {
       res.render("bets", {
-        data: allBets
+        data: allBets,
       });
     });
   });
