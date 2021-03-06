31d3b8197e84ab9e46373bb6298460440fe50894
jayarghargh <jayarghargh@gmail.com>
2021-03-07 08:55:41 +1030

    removed redundant field from form

--- public/js/member-lookup.js
+++ public/js/member-lookup.js
@@ -1,6 +1,4 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
   $.get("/api/members/getUsernames").then(data => {
     // let objData = Object.entries(data);
     const namesArray = [];
@@ -9,9 +7,7 @@ $(document).ready(() => {
     });
     // $(".member-name").text(data.email);
     $(".bet-against").autocomplete({
-      /*refer to the same id that input has*/
       source: namesArray
-      /*set the source name that you gave your array variable*/
     });
   });
 });
