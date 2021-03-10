e14b23eb7aded1d6562f3a80c6ae961d034412a3
Nathan Opresnik <nopresnik@me.com>
2021-03-11 10:15:41 +1100

    Fix routes and create routes for about and faq.  Add card to faq and about page.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -20,25 +20,20 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  app.get("/createbet", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/");
-    }
+  app.get("/createbet", isAuthenticated, (req, res) => {
     res.render("createbet");
   });
 
   app.get("/allbets", (req, res) => {
     // Users can view all existing bets
-    if (req.user) {
-      res.redirect("/");
-    }
     res.render("allbets");
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/faqs", (req, res) => {
+    res.render("faqs");
+  });
+
+  app.get("/aboutus", (req, res) => {
+    res.render("aboutUs");
   });
 };
